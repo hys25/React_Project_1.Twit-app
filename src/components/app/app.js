4ad495c25e1dbd1ca23434d39e83cfae504bc96a
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-07-15 09:51:22 +0300

    important, like

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from '../post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: "Going to learn React", important: false, id:'dfvf'},
+        {label: "Thai is so good", important: false, id:'fdvdf'},
+        {label: "I need a break...", important: true, id:'ewve'}
+    ];
+
     return (
        <div className="app">
             <AppHeader/>
@@ -16,7 +23,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts = {data}/>
             <PostAddForm/>
        </div>
     )
