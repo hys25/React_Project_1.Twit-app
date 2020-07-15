f536d16d7f698a55c7d85f67027a6734d0e5b5ba
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-07-15 13:23:00 +0300

    add/delete items

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import './post-list-item.css';
+import './post-list-item.css'
 
 export default class PostListItem extends Component {
     constructor(props) {
@@ -8,9 +8,9 @@ export default class PostListItem extends Component {
         this.state = {
             important: false,
             like: false
-        }
-        this.onImportant = this.onImportant.bind(this); // Или используем другой способ - мы их будем еще применять в следующих проектах
-        this.onLike = this.onLike.bind(this); 
+        };
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);
     }
 
     onImportant() {
@@ -26,38 +26,41 @@ export default class PostListItem extends Component {
     }
 
     render() {
-        const {label} = this.props;
+        const {label, onDelete} = this.props;
         const {important, like} = this.state;
+        let classNames = 'app-list-item d-flex justify-content-between';
 
-        let classNames = "app-list-item d-flex justify-content-between";
         if (important) {
-            classNames += ' important';
+            classNames +=' important';
         }
+
         if (like) {
-            classNames += ' like';
+            classNames +=' like';
         }
-    return (
-        <div className={classNames}>
-            <span 
+
+        return (
+            <div className={classNames}>
+                <span 
                 className="app-list-item-label"
                 onClick={this.onLike}>
-                {label}
-            </span>
-            <div className="d-flex justify-content-center align-items-center">
-                <button 
-                type="button" 
-                className="btn-star btn-sm"
-                onClick={this.onImportant}>
-                    <i className="fa fa-star"></i>
-                </button>
-                <button 
-                type="button" 
-                className="btn-trash btn-sm">
-                    <i className="fa fa-trash-o"></i>
-                </button>
-                <i className="fa fa-heart"></i>
+                    {label}
+                </span>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button 
+                    type="button" 
+                    className="btn-star btn-sm"
+                    onClick={this.onImportant}>
+                        <i className="fa fa-star"></i>
+                    </button>
+                    <button 
+                        type="button" 
+                        className="btn-trash btn-sm"
+                        onClick={onDelete}>
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                    <i className="fa fa-heart"></i>
+                </div>
             </div>
-        </div>
         )
-    }
+        }
 }
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,31 +3,22 @@ import React from 'react';
 import PostListItem from '../post-list-item';
 import './post-list.css'
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
 
-    const elements = posts.map( (item) => {
-        // Простой способ проверки на объект + содержится ли в нем информация
-        if ( typeof item === 'object' && isEmpty(item) ){ 
-            const {id, ...itemProps} = item;
-            return (
-                <li key = {id} className='list-group-item'>
-                    <PostListItem {...itemProps}/>
-                </li>
-            )
-        }
-    })
-
-    function isEmpty(obj) {
-        for(let key in obj)
-        {
-            return true;
-        }
-        return false;
-    }
+    const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
+        return (
+            <li key={id} className='list-group-item'>
+                <PostListItem 
+                {...itemProps}
+                onDelete={() => onDelete(id)}/>
+            </li>
+        )
+    });
 
     return (
         <ul className="app-list list-group">
-           {elements}
+            {elements}
         </ul>
     )
 }
